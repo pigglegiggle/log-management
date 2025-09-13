b216a9439925f8d39e2ffa01771ff054586f26c8
Sin <101711627+pigglegiggle@users.noreply.github.com>
2025-09-13 14:40:30 +0700

    Rename log file from crowdstrike.json to tenant.json

--- samples/send_tenant.sh
+++ samples/send_tenant.sh
@@ -4,11 +4,11 @@ API_URL="http://localhost:3000/ingest"
 
 # Get the directory where this script is located
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-LOG_FILE="$SCRIPT_DIR/crowdstrike.json"
+LOG_FILE="$SCRIPT_DIR/tenant.json"
 
 curl -s -X POST "$API_URL" \
      -H "Content-Type: application/json" \
      -d @"$LOG_FILE"
 
 echo
-echo "Log from $LOG_FILE sent successfully"
+echo "Logs from $LOG_FILE sent successfully"
